8ef235a2b837af4d7bf3ac7d6eec837a609ae4e9
diana_yang <dyang20@stuy.edu>
2020-11-19 17:46:05 -0500

    get commands?

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,3 +1,24 @@
 public class RationalNumber extends RealNumber{
+  private int numerator, denominator;
 
+  public RationalNumber(int nume, int deno){
+    if (deno==0){
+      nume = 0;
+      deno= 1;
+    }
+    numerator = nume;
+    denominator = deno;
+  }
+
+  public double getValue(){
+    return (double) (numerator / denominator);
+  }
+
+  public int getNumerator(){
+    return numerator;
+  }
+
+  public int getDenominator(){
+    return denominator;
+  }
 }
