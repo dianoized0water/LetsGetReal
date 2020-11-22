4e90ff312a0ea7448fe0cf4999c3aa7ccfc4c27b
diana_yang <dyang20@stuy.edu>
2020-11-21 19:10:49 -0500

    added equals

--- RealNumber.java
+++ RealNumber.java
@@ -9,6 +9,14 @@ public class RealNumber{
     return value;
   }
 
+  public boolean equals(RealNumber other){
+    double a = value - other.value;
+    if (Math.abs(a)> (0.00001*value)){
+      return false;
+    }
+    return true;
+  }
+
   public RealNumber add(RealNumber other){
     RealNumber c = new RealNumber(this.value + other.value);
     return c;
