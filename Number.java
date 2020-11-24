803de7032b71748ff4b6ccd9018e778290fe3a74
diana_yang <dyang20@stuy.edu>
2020-11-23 22:42:06 -0500

    sighs

--- Number.java
+++ Number.java
@@ -2,10 +2,17 @@ public abstract class Number{
   public abstract double getValue();
 
   public int compareTo(Number other){
-
+    return (int) (this.getValue()-other.getValue());
   }
 
   public boolean equals(Number other){
-    
+    double a = this.getValue();
+    double b = other.getValue();
+    if ((a==0) || (b==0)){
+      return (a==b);
+    }
+    else {
+      return ((Math.abs(a-b)*100 / Math.abs(a+b)/2) < 0.001);
+    }
   }
 }
