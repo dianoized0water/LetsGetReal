9cdbb9234ddcffc99a3745bbd0f76c156e41f147
diana_yang <dyang20@stuy.edu>
2020-11-29 13:04:19 -0500

    .

--- RationalNumber.java
+++ RationalNumber.java
@@ -2,7 +2,6 @@ public class RationalNumber extends RealNumber{
   private int numerator, denominator;
 
   public RationalNumber(int nume, int deno){
-    super(0.0);
     if (deno==0){
       nume = 0;
       deno= 1;
