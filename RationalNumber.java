1b8b5d41329f5164322905add6f3a8dfe6f038d3
diana_yang <dyang20@stuy.edu>
2020-12-23 20:18:56 -0500

    fixed constructor

--- RationalNumber.java
+++ RationalNumber.java
@@ -2,6 +2,7 @@ public class RationalNumber extends RealNumber{
   private int numerator, denominator;
 
   public RationalNumber(int nume, int deno){
+    super(0.0);
     if (deno==0){
       nume = 0;
       deno= 1;
